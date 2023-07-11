1a56f28bb064b46cef808005c4354a4a23b48e58
tbrown034 <tbrown034@gmail.com>
2023-07-11 15:13:36 -0500

    WIP(Game): achievements are now listed and a bonus button apperas, but disapperas)

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -10,18 +10,60 @@ import BonusButton from "./BonusButton";
 function Game() {
   const [distance, setDistance] = useState(0);
   const [isLaunched, setIsLaunched] = useState(false);
+  const [currentMilestoneIndex, setCurrentMilestoneIndex] = useState(0);
+  const [bonusAvailable, setBonusAvailable] = useState(false);
+
   const [milestones, setMilestones] = useState([
     { distance: 100, message: "Cleared Earth's atmosphere", boost: 10 },
     { distance: 400, message: "Reached the thermosphere", boost: 20 },
-    // Add more milestones here...
+    { distance: 1000, message: "Entered low Earth orbit (LEO)", boost: 30 },
+    {
+      distance: 2000,
+      message: "Completed one full orbit around Earth",
+      boost: 40,
+    },
+    { distance: 2500, message: "Crossed the Kármán line", boost: 50 },
+    { distance: 3000, message: "Entered the mesosphere", boost: 60 },
+    { distance: 4000, message: "Reached the stratopause", boost: 70 },
+    { distance: 5000, message: "Entered the ionosphere", boost: 80 },
+    { distance: 6000, message: "Reached the exosphere", boost: 90 },
+    { distance: 7000, message: "Crossed the magnetopause", boost: 100 },
+    {
+      distance: 8000,
+      message: "Passed through the Van Allen radiation belts",
+      boost: 110,
+    },
+    {
+      distance: 9000,
+      message: "Reached escape velocity from Earth's gravitational pull",
+      boost: 120,
+    },
+    { distance: 10000, message: "Entered heliocentric orbit", boost: 130 },
+    { distance: 12000, message: "Crossed the orbit of Venus", boost: 140 },
+    { distance: 14000, message: "Crossed the orbit of Mars", boost: 150 },
+    { distance: 16000, message: "Crossed the asteroid belt", boost: 160 },
+    {
+      distance: 18000,
+      message: "Entered Jupiter's gravitational influence",
+      boost: 170,
+    },
+    { distance: 20000, message: "Crossed the orbit of Saturn", boost: 180 },
+    {
+      distance: 22000,
+      message: "Entered Uranus's gravitational influence",
+      boost: 190,
+    },
+    { distance: 24000, message: "Crossed the orbit of Neptune", boost: 200 },
   ]);
-
   const boost = () => {
     setDistance(distance + 10);
   };
 
   const bonusBoost = () => {
-    setDistance(distance + 100);
+    // Use the boost from the current milestone
+    setDistance(distance + milestones[currentMilestoneIndex].boost);
+    // Set bonusAvailable to false
+    setBonusAvailable(false);
   };
 
   const back = () => {
@@ -37,22 +79,36 @@ function Game() {
     let interval;
     if (isLaunched) {
       interval = setInterval(() => {
-        setDistance((distance) => distance + 1);
+        setDistance((distance) => {
+          // Check if the rocket has reached the next milestone
+          if (
+            currentMilestoneIndex < milestones.length &&
+            distance >= milestones[currentMilestoneIndex].distance
+          ) {
+            // Update the currentMilestoneIndex
+            setCurrentMilestoneIndex(currentMilestoneIndex + 1);
+            // Set bonusAvailable to true
+            setBonusAvailable(true);
+          }
+          return distance + 1;
+        });
       }, 1000);
     } else if (!isLaunched && distance !== 0) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [isLaunched, distance]);
+  }, [isLaunched, distance, currentMilestoneIndex, milestones]);
 
   return (
     <div className="flex flex-col gap-20 py-20">
       {isLaunched ? (
         <>
           <Status distance={distance} />
-          <Achievements milestones={milestones} />
-          <BonusButton onClick={bonusBoost} />
-          <div className="flex gap-8 flex-col ">
+          <Achievements
+            milestones={milestones.slice(0, currentMilestoneIndex)}
+          />
+          {bonusAvailable && <BonusButton onClick={bonusBoost} />}
+          <div className="flex flex-col gap-8 ">
             <BoostButton onClick={boost} />
             <BackButton onClick={back} />
           </div>
